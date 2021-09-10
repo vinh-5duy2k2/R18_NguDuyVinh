c58813df7936082a23614343db6ba85e3766668d
vinh-5duy2k2 <vinhchimlon@gmail.com>
2021-09-10 10:05:05 +0700

    Testingsystem_Assignment_Update

--- SQL/Testing_System_Assignment_3.sql
+++ SQL/Testing_System_Assignment_3.sql
@@ -1,14 +1,15 @@
 USE testing_system;
 -- Question1
--- Question2
 
+
+-- Question2
 SELECT * 
 FROM Department;
 
 -- Question3
 SELECT DepartmentID
 FROM Department
-WHERE DepartmentName = 'Ban hang';
+WHERE DepartmentName = 'Sale';
 
 -- Question4
 SELECT 
@@ -28,19 +29,19 @@ LIMIT 1;
 -- Question6
 SELECT groupName
 FROM `Group`
-WHERE CreateDate < '2018-12-20';
+WHERE CreateDate < '2019-12-20';
 
 
 -- Question7
 SELECT QuestionID
 FROM Answer
 GROUP BY QuestionID
-HAVING Count(QuestionID) > 4;
+HAVING Count(QuestionID) >= 4;
 
 -- Question8
 SELECT `Code`
 FROM Exam
-WHERE Duration >= '01:00:00' AND CreateDate < '2020-11-20';
+WHERE Duration >= '01:00:00' AND CreateDate < '2019-12-20';
 
 -- Question9
 SELECT *
@@ -58,11 +59,29 @@ WHERE DepartmentID = 2;
 -- Question11
 SELECT *
 FROM `Account`
-WHERE FullName LIKE 'N%' AND FullName LIKE '%h'; 
+WHERE FullName LIKE 'D%o' ; 
+
 
 -- Question12
+DELETE
+FROM exam
+WHERE CreateDate < '2019-12-20';
 
--- Question5
--- Question5
--- Question5
 
+-- Question13
+DELETE
+FROM Question
+WHERE content LIKE 'cau hoi%';
+
+-- Question14
+UPDATE `Account`
+SET 
+	FullName = 'Nguyen Ba Loc',
+    email = 'loc.nguyenba@vti.com.vn'
+WHERE AccountID =5;
+
+
+-- Question15
+UPDATE groupaccount
+SET GroupID = 4
+WHERE AccountID = 5;
